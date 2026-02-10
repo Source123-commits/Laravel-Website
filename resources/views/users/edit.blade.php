34451c2d4ceb79fe3c867f2fc6447b5a85fc5ff6
Sakib Ahmed <sakibahmednadim2050@gmail.com>
2026-02-10 09:40:18 +0600

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
          <div class="flex justify-between ">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Usera / Edit
+            Users / Edit
         </h2>
         
         <a href="{{route('users.index')}}" class="bg-slate-700 text-sm px-3 py-2 rounded-md text-white ">Back</a>
